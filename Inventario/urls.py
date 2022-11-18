cf0269aada75092f223c6e8844deabed5c4b7bc2
Eliseo Chavez Montiel <gnuchavez@gmail.com>
2022-11-18 00:39:47 -0600

    DELETE
    Eliminación de archivos viejos

--- Inventario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path #Importamos Path
-from Inventario.views import * #Importamos todas las vistas
-
-
-#El archivo debe tener un archivo llamado exactamente urlpattern
-urlpatterns = [
-    path("",inicio, name="inventario-inicio"),
-    path("alta_laptop/",alta_laptop,name="alta_laptop"),
-]
